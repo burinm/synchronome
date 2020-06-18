8bb52028cec3921c4b264217dd8024e0b754500a
burin <21989631+burinm@users.noreply.github.com>
2020-06-18 08:26:47 -0600

    Fixed dos LF line endings, removed tabs..grrr

--- feasibility/feasibility_tests.c
+++ feasibility/feasibility_tests.c
@@ -30,91 +30,91 @@ int scheduling_point_feasibility(U32_T numServices, U32_T period[], U32_T wcet[]
 
 
 int main(void)
-{ 
+{
     int i;
-	U32_T numServices;
-    
+    U32_T numServices;
+
     printf("******** Completion Test Feasibility Example\n");
-   
+
     printf("Ex-0 U=%4.2f (C1=1, C2=1, C3=2; T1=2, T2=10, T3=15; T=D): ",
-		   ((1.0/2.0) + (1.0/10.0) + (2.0/15.0)));
-	numServices = sizeof(ex0_period)/sizeof(U32_T);
+           ((1.0/2.0) + (1.0/10.0) + (2.0/15.0)));
+    numServices = sizeof(ex0_period)/sizeof(U32_T);
     if(completion_time_feasibility(numServices, ex0_period, ex0_wcet, ex0_period) == TRUE)
         printf("FEASIBLE\n");
     else
         printf("INFEASIBLE\n");
 
-    printf("Ex-1 U=%4.2f (C1=1, C2=1, C3=2; T1=2, T2=5, T3=7; T=D): ", 
-		   ((1.0/2.0) + (1.0/5.0) + (1.0/7.0)));
-	numServices = sizeof(ex1_period)/sizeof(U32_T);
+    printf("Ex-1 U=%4.2f (C1=1, C2=1, C3=2; T1=2, T2=5, T3=7; T=D): ",
+           ((1.0/2.0) + (1.0/5.0) + (1.0/7.0)));
+    numServices = sizeof(ex1_period)/sizeof(U32_T);
     if(completion_time_feasibility(numServices, ex1_period, ex1_wcet, ex1_period) == TRUE)
         printf("FEASIBLE\n");
     else
         printf("INFEASIBLE\n");
-	
+
     printf("Ex-2 U=%4.2f (C1=1, C2=1, C3=1, C4=2; T1=2, T2=5, T3=7, T4=13; T=D): ",
-		   ((1.0/2.0) + (1.0/5.0) + (1.0/7.0) + (2.0/13.0)));
-	numServices = sizeof(ex2_period)/sizeof(U32_T);
+           ((1.0/2.0) + (1.0/5.0) + (1.0/7.0) + (2.0/13.0)));
+    numServices = sizeof(ex2_period)/sizeof(U32_T);
     if(completion_time_feasibility(numServices, ex2_period, ex2_wcet, ex2_period) == TRUE)
         printf("FEASIBLE\n");
     else
         printf("INFEASIBLE\n");
 
     printf("Ex-3 U=%4.2f (C1=1, C2=2, C3=3; T1=3, T2=5, T3=15; T=D): ",
-		   ((1.0/3.0) + (2.0/5.0) + (3.0/15.0)));
-	numServices = sizeof(ex3_period)/sizeof(U32_T);
+           ((1.0/3.0) + (2.0/5.0) + (3.0/15.0)));
+    numServices = sizeof(ex3_period)/sizeof(U32_T);
     if(completion_time_feasibility(numServices, ex3_period, ex3_wcet, ex3_period) == TRUE)
         printf("FEASIBLE\n");
     else
         printf("INFEASIBLE\n");
-	
+
     printf("Ex-4 U=%4.2f (C1=1, C2=1, C3=4; T1=2, T2=4, T3=16; T=D): ",
-		   ((1.0/2.0) + (1.0/4.0) + (4.0/16.0)));
-	numServices = sizeof(ex4_period)/sizeof(U32_T);
+           ((1.0/2.0) + (1.0/4.0) + (4.0/16.0)));
+    numServices = sizeof(ex4_period)/sizeof(U32_T);
     if(completion_time_feasibility(numServices, ex4_period, ex4_wcet, ex4_period) == TRUE)
         printf("FEASIBLE\n");
     else
         printf("INFEASIBLE\n");
 
 
-	printf("\n\n");
+    printf("\n\n");
     printf("******** Scheduling Point Feasibility Example\n");
 
     printf("Ex-0 U=%4.2f (C1=1, C2=1, C3=2; T1=2, T2=10, T3=15; T=D): ",
-		   ((1.0/2.0) + (1.0/10.0) + (2.0/15.0)));
-	numServices = sizeof(ex0_period)/sizeof(U32_T);
+           ((1.0/2.0) + (1.0/10.0) + (2.0/15.0)));
+    numServices = sizeof(ex0_period)/sizeof(U32_T);
     if(scheduling_point_feasibility(numServices, ex0_period, ex0_wcet, ex0_period) == TRUE)
         printf("FEASIBLE\n");
     else
         printf("INFEASIBLE\n");
 
-    printf("Ex-1 U=%4.2f (C1=1, C2=1, C3=2; T1=2, T2=5, T3=7; T=D): ", 
-		   ((1.0/2.0) + (1.0/5.0) + (1.0/7.0)));
-	numServices = sizeof(ex1_period)/sizeof(U32_T);
+    printf("Ex-1 U=%4.2f (C1=1, C2=1, C3=2; T1=2, T2=5, T3=7; T=D): ",
+           ((1.0/2.0) + (1.0/5.0) + (1.0/7.0)));
+    numServices = sizeof(ex1_period)/sizeof(U32_T);
     if(scheduling_point_feasibility(numServices, ex1_period, ex1_wcet, ex1_period) == TRUE)
         printf("FEASIBLE\n");
     else
         printf("INFEASIBLE\n");
-	
+
     printf("Ex-2 U=%4.2f (C1=1, C2=1, C3=1, C4=2; T1=2, T2=5, T3=7, T4=13; T=D): ",
-		   ((1.0/2.0) + (1.0/5.0) + (1.0/7.0) + (2.0/13.0)));
-	numServices = sizeof(ex2_period)/sizeof(U32_T);
+           ((1.0/2.0) + (1.0/5.0) + (1.0/7.0) + (2.0/13.0)));
+    numServices = sizeof(ex2_period)/sizeof(U32_T);
     if(scheduling_point_feasibility(numServices, ex2_period, ex2_wcet, ex2_period) == TRUE)
         printf("FEASIBLE\n");
     else
         printf("INFEASIBLE\n");
 
     printf("Ex-3 U=%4.2f (C1=1, C2=2, C3=3; T1=3, T2=5, T3=15; T=D): ",
-		   ((1.0/3.0) + (2.0/5.0) + (3.0/15.0)));
-	numServices = sizeof(ex3_period)/sizeof(U32_T);
+        ((1.0/3.0) + (2.0/5.0) + (3.0/15.0)));
+    numServices = sizeof(ex3_period)/sizeof(U32_T);
     if(scheduling_point_feasibility(numServices, ex3_period, ex3_wcet, ex3_period) == TRUE)
         printf("FEASIBLE\n");
     else
         printf("INFEASIBLE\n");
-	
+
     printf("Ex-4 U=%4.2f (C1=1, C2=1, C3=4; T1=2, T2=4, T3=16; T=D): ",
-		   ((1.0/2.0) + (1.0/4.0) + (4.0/16.0)));
-	numServices = sizeof(ex4_period)/sizeof(U32_T);
+           ((1.0/2.0) + (1.0/4.0) + (4.0/16.0)));
+    numServices = sizeof(ex4_period)/sizeof(U32_T);
     if(scheduling_point_feasibility(numServices, ex4_period, ex4_wcet, ex4_period) == TRUE)
         printf("FEASIBLE\n");
     else
@@ -127,52 +127,52 @@ int completion_time_feasibility(U32_T numServices, U32_T period[], U32_T wcet[],
 {
   int i, j;
   U32_T an, anext;
-  
+
   // assume feasible until we find otherwise
   int set_feasible=TRUE;
-   
+
   //printf("numServices=%d\n", numServices);
-  
+
   for (i=0; i < numServices; i++)
   {
        an=0; anext=0;
-       
+
        for (j=0; j <= i; j++)
        {
            an+=wcet[j];
        }
-       
-	   //printf("i=%d, an=%d\n", i, an);
+
+       //printf("i=%d, an=%d\n", i, an);
 
        while(1)
        {
              anext=wcet[i];
-	     
+
              for (j=0; j < i; j++)
                  anext += ceil(((double)an)/((double)period[j]))*wcet[j];
-		 
+
              if (anext == an)
                 break;
              else
                 an=anext;
 
-			 //printf("an=%d, anext=%d\n", an, anext);
+             //printf("an=%d, anext=%d\n", an, anext);
        }
-       
-	   //printf("an=%d, deadline[%d]=%d\n", an, i, deadline[i]);
+
+       //printf("an=%d, deadline[%d]=%d\n", an, i, deadline[i]);
 
        if (an > deadline[i])
        {
           set_feasible=FALSE;
        }
   }
-  
+
   return set_feasible;
 }
 
 
-int scheduling_point_feasibility(U32_T numServices, U32_T period[], 
-								 U32_T wcet[], U32_T deadline[])
+int scheduling_point_feasibility(U32_T numServices, U32_T period[],
+                                U32_T wcet[], U32_T deadline[])
 {
    int rc = TRUE, i, j, k, l, status, temp;
 
@@ -180,7 +180,7 @@ int scheduling_point_feasibility(U32_T numServices, U32_T period[],
    {
       status=0;
 
-      for (k=0; k<=i; k++) 
+      for (k=0; k<=i; k++)
       {
           for (l=1; l <= (floor((double)period[i]/(double)period[k])); l++)
           {
@@ -189,10 +189,10 @@ int scheduling_point_feasibility(U32_T numServices, U32_T period[],
                for (j=0; j<=i; j++) temp += wcet[j] * ceil((double)l*(double)period[k]/(double)period[j]);
 
                if (temp <= (l*period[k]))
-			   {
-				   status=1;
-				   break;
-			   }
+               {
+                   status=1;
+                   break;
+               }
            }
            if (status) break;
       }
