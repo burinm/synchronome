46b73d5708e526be13b027b127a913a9f882825c
burin <21989631+burinm@users.noreply.github.com>
2020-07-19 13:25:31 -0600

    Box added to motion detector

--- simple-capture/motion.c
+++ simple-capture/motion.c
@@ -10,11 +10,26 @@ assert(first->size == second->size);
     //TODO - use words instead of bytes?
     unsigned char* f = first->start; 
     unsigned char* s = second->start; 
+
 // YUYV
-        
+
 #if 1
+box_t scan_area = { 213, 160, 426, 320 };
+
+    for (int i=scan_area.h1; i < scan_area.h2; i++, f += X_RES, s+= X_RES) { //every rom
+        for (int j=scan_area.w1; j < scan_area.w2; j+=2, f+=2, s+=2) { //every other column (Y)
+            if (*f != *s) {
+                count++;
+            }
+        }
+    }
+#endif
+
+
+#if 0
     for (int i=0; i<first->size /2; i++) {
-        if (((*f) & 0xf0) != ((*s) & 0xf0)) {
+        //if (((*f) & 0xf0) != ((*s) & 0xf0)) {
+        if (*f != *s) {
             count++;
         }
         //Just diff Ys, skip U/V
